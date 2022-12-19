12c3feee1109527134df3d24bd4eed1a831f284d
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-19 01:39:36 +0100

    Section 5 - keywords and expressions

---
+++ src/section5/keyWordsAndExpressions/Main.java
@@ -0,0 +1,23 @@
+package section5.keyWordsAndExpressions;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double kilometers = (100*1.609344);
+
+
+        int highScore = 50;
+
+        if(highScore>25){
+            highScore=1000+highScore;
+        }
+
+        int health = 100;
+
+        if((health<25) && (highScore>25)){
+            highScore=highScore-100;
+        }
+
+    }
+
+}
