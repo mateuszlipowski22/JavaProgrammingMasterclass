703bcf1d5b23366e74bc6e8c6777d32aafe9608c
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-20 00:54:46 +0100

    Section 5 - methods

---
+++ src/section5/ifKeywordAndCodeBlocks/MainChallenge.java
@@ -0,0 +1,34 @@
+package section5.ifKeywordAndCodeBlocks;
+
+public class MainChallenge {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+
+        System.out.println("The highScore is " + highScore);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        System.out.println("The next highScore is " +calculateScore(true, 10000, 8, 200));
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+
+        int finalScore = score;
+
+        if(gameOver){
+            finalScore += (levelCompleted * bonus);
+        }
+
+        return finalScore;
+    }
+
+}
