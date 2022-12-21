f7bea733fb894a8f02673b52b4c2e9a20c8a8d43
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-21 01:34:56 +0100

    Section 6 - Switch statement enhanced

--- src/section6/switchStatement/Main.java
+++ src/section6/switchStatement/Main.java
@@ -14,7 +14,12 @@ public class Main {
 //            System.out.println("Value was not 1 or 2");
 //        }
 
-    int switchValue = 1;
+//        String month = "APRIL";
+        String month = "sss";
+
+        System.out.println(month + " is in the " + getQuarterEnhanced(month)+ " quarter");
+
+        int switchValue = 1;
 
     switch (switchValue){
         case 1:
@@ -31,6 +36,52 @@ public class Main {
             System.out.println("Value was not 1, 2, 3, 4 and 5");
             break;
     }
+
+        switch (switchValue) {
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
+                System.out.println("Was 3, 4 or 5");
+                System.out.println("Actually it was " + switchValue);
+            }
+            default -> System.out.println("Value was not 1, 2, 3, 4 and 5");
+        }
+
+    }
+
+    public static String getQuarter(String month){
+        switch (month){
+            case "JANUARY":
+            case "FEBRUARY":
+            case "MARCH":
+                return "1st";
+            case "APRIL":
+            case "MAY":
+            case "JUNE":
+                return "2nd";
+            case "JULY":
+            case "AUGUST":
+            case "SEPTEMBER":
+                return "3rd";
+            case "OCTOBER":
+            case "NOVEMBER":
+            case "DECEMBER":
+                return "4th";
+        }
+
+        return "BAD";
+    }
+
+    public static String getQuarterEnhanced(String month){
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> {
+                yield  month + " is BAD";
+            }
+        };
 
     }
 
