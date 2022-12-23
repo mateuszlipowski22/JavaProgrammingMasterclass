c296b24b49d8d6eb2b2cf5a925210d1e5dc16e26
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-23 01:25:50 +0100

    Section 6 - sumFirstAndLastDigit challenge

--- src/section6/whileLoop/Challenges.java
+++ src/section6/whileLoop/Challenges.java
@@ -4,9 +4,15 @@ public class Challenges {
 
     public static void main(String[] args) {
 
-        System.out.println("isPalindrome(-1221) = " + isPalindrome(-1221));
-        System.out.println("isPalindrome(707) = " + isPalindrome(707));
-        System.out.println("isPalindrome(11212) = " + isPalindrome(11212));
+//        System.out.println("isPalindrome(-1221) = " + isPalindrome(-1221));
+//        System.out.println("isPalindrome(707) = " + isPalindrome(707));
+//        System.out.println("isPalindrome(11212) = " + isPalindrome(11212));
+
+        System.out.println("sumFirstAndLastDigit(252) = " + sumFirstAndLastDigit(252));
+        System.out.println("sumFirstAndLastDigit(257) = " + sumFirstAndLastDigit(257));
+        System.out.println("sumFirstAndLastDigit(0) = " + sumFirstAndLastDigit(0));
+        System.out.println("sumFirstAndLastDigit(5) = " + sumFirstAndLastDigit(5));
+        System.out.println("sumFirstAndLastDigit(-10) = " + sumFirstAndLastDigit(-10));
     }
 
     public static boolean isPalindrome(int number){
@@ -24,5 +30,20 @@ public class Challenges {
 
         return reverse==savedNumber;
 
+    }
+
+    public static int sumFirstAndLastDigit(int number){
+
+        if(number<0){
+            return -1;
+        }
+
+        int sum = number%10;
+
+        while(number>9){
+            number/=10;
+        }
+
+        return sum+number;
     }
 }
