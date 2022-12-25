cb7043ad43bf8dbeae4cb2c1fb03ce90e6bde4ca
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-25 23:08:09 +0100

    Section 6 - adding getting and setting methods

--- src/section7/classesPart1/Main.java
+++ src/section7/classesPart1/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Car car = new Car();
+        car.describeCar();
+
+        System.out.println(car.getMake());
+
     }
 
 }
