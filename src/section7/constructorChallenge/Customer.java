47847d2eb38fb027447891c5a8ca6d28ba77886b
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-27 01:20:02 +0100

    Section 6 - Customer challenge

---
+++ src/section7/constructorChallenge/Customer.java
@@ -0,0 +1,46 @@
+package section7.constructorChallenge;
+
+public class Customer {
+
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public Customer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public Customer() {
+        this("Default name", "default email");
+    }
+
+    public Customer(String name, String email) {
+        this(name, 0, email);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(double creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/section7/constructorChallenge/Main.java
@@ -0,0 +1,16 @@
+package section7.constructorChallenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Customer customer = new Customer();
+
+        System.out.println("customer.getName() = " + customer.getName());
+        System.out.println("customer.getEmail() = " + customer.getEmail());
+        System.out.println("customer.getCreditLimit() = " + customer.getCreditLimit());
+
+
+    }
+
+}
