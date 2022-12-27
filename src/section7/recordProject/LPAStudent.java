013cf2c4b70efff2d2c627e0a147ffb1b22a31ae
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-27 01:55:23 +0100

    Section 6 - record

---
+++ src/section7/recordProject/LPAStudent.java
@@ -0,0 +1,4 @@
+package section7.recordProject;
+
+public record LPAStudent(String id, String name, String dateOfBirth, String classList) {
+}
--- src/section7/recordProject/Main.java
+++ src/section7/recordProject/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 1; i <=5 ; i++) {
-            Student s = new Student("90219"+i,
+            LPAStudent s = new LPAStudent("90219"+i,
                     switch (i){
                         case 1 -> "Mary";
                         case 2 -> "Carol";
@@ -19,6 +19,17 @@ public class Main {
 
         }
 
+        Student pojoStudent = new Student ("912106", "Ann", "05/11/1985", "Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent ("912107", "Bill", "05/11/1985", "Java Masterclass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList()+", Java OCP Exam 829");
+//        recordStudent.setClassList(recordStudent.classList()+", Java OCP Exam 829");
+
+        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList());
 
     }
 
