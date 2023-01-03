7b88e292d1a5f9a7483cff968279dc91c9747758
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-03 01:33:59 +0100

    Section 8 - polymorphism #2 and factory design pattern in action

--- src/section8/polymorphism/Main.java
+++ src/section8/polymorphism/Main.java
@@ -1,11 +1,28 @@
 package section8.polymorphism;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Movie theMovie = new Adventure("Star Wars");
-        theMovie.watchMovie();
+//        Movie theMovie = Movie.getMovie("Science", "Star Wars");
+//        theMovie.watchMovie();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Enter type (A for Adventure, C for Comedy, S for Science Fiction or Q for quit):");
+            String type = scanner.nextLine();
+
+            if("Qq".contains(type)){
+                break;
+            }
+            System.out.println("Enter Movie Title");
+            String title = scanner.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
 
     }
 
