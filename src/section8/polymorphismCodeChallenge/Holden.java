b6b91605ad88bb3223fe7b6bd28e346db6fc9bcc
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-04 01:03:13 +0100

    Section 8 - polymorphism Code Challenge

---
+++ src/section8/polymorphismCodeChallenge/Holden.java
@@ -0,0 +1,24 @@
+package section8.polymorphismCodeChallenge;
+
+public class Holden extends Car{
+
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+//    @Override
+//    public String startEngine() {
+//        return super.startEngine();
+//    }
+//
+//    @Override
+//    public String accelerate() {
+//        return super.accelerate();
+//    }
+//
+//    @Override
+//    public String brake() {
+//        return super.brake();
+//    }
+
+}
