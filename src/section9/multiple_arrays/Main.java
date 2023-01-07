359b577d648dc85abf9ed2a2fa20b0efb1d6730f
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-07 22:34:37 +0100

    Section 9 - multiple arrays #2

--- src/section9/multiple_arrays/Main.java
+++ src/section9/multiple_arrays/Main.java
@@ -15,10 +15,10 @@ public class Main {
         }
 
         for (int i = 0; i < array2.length; i++) {
-            var innerArray =array2[i];
+            var innerArray = array2[i];
             for (int j = 0; j < innerArray.length; j++) {
 //                System.out.print(array2[i][j]+" ");
-                array2[i][j]=i*10+j;
+                array2[i][j] = i * 10 + j;
             }
 //            System.out.println();
         }
@@ -30,9 +30,30 @@ public class Main {
 //            System.out.println();
 //        }
 
-        System.out.println("Arrays.deepToString(array2) = " + Arrays.deepToString(array2));
+        System.out.println(Arrays.deepToString(array2));
+        array2[1] = new int[]{1, 2, 3};
+        System.out.println(Arrays.deepToString(array2));
 
+        Object[] anyArray = new Object[3];
+        System.out.println(Arrays.toString(anyArray));
+        anyArray[0] = new String[]{"a", "b", "c"};
+        System.out.println(Arrays.toString(anyArray));
 
+        anyArray[1] = new String[][]{
+                {"1", "2"},
+                {"3", "4", "5"},
+                {"5", "6", "7", "8"},
+        };
+        System.out.println(Arrays.deepToString(anyArray));
+
+        anyArray[2] = new int[2][2][2];
+        System.out.println(Arrays.deepToString(anyArray));
+
+        for(Object object : anyArray){
+            System.out.println("object.getClass().getSimpleName() = " + object.getClass().getSimpleName());
+            System.out.println("object.toString() = " + object);
+            System.out.println("Arrays.deepToString(object) = " + Arrays.deepToString((Object[]) object));
+        }
     }
 
 }
