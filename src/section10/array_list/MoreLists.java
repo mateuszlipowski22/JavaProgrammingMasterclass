8eb5f08965a4b6f3120ff2eb378d2ca5cd507bb4
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-08 02:03:37 +0100

    Section 10 - array list #3

--- src/section10/array_list/MoreLists.java
+++ src/section10/array_list/MoreLists.java
@@ -1,6 +1,8 @@
 package section10.array_list;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class MoreLists {
@@ -21,6 +23,45 @@ public class MoreLists {
 
         groceries.addAll(nextList);
         System.out.println(groceries);
+
+        System.out.println("Third element = " + groceries.get(2));
+
+        if(groceries.contains("mustard")){
+            System.out.println("List contains mustard");
+        }
+
+        groceries.add("yougurt");
+        System.out.println("groceries.indexOf(\"yougurt\") = " + groceries.indexOf("yougurt"));
+        System.out.println("groceries.lastIndexOf(\"yougurt\") = " + groceries.lastIndexOf("yougurt"));
+
+        System.out.println(groceries);
+        groceries.remove(1);
+        System.out.println(groceries);
+        groceries.remove("yougurt");
+        System.out.println(groceries);
+
+        groceries.removeAll(List.of("apples","milk"));
+        System.out.println(groceries);
+
+        groceries.retainAll(List.of("apples", "milk", "mustard", "cheese"));
+        System.out.println(groceries);
+
+        groceries.clear();
+        System.out.println(groceries);
+        System.out.println("isEmpty = "+ groceries.isEmpty());
+
+        groceries.addAll(List.of("apples", "milk", "mustard", "cheese"));
+        groceries.addAll(Arrays.asList("pickles", "mustard", "cheese"));
+
+        System.out.println(groceries);
+        groceries.sort(Comparator.naturalOrder());
+        System.out.println(groceries);
+
+        groceries.sort(Comparator.reverseOrder());
+        System.out.println(groceries);
+
+        var groceryArray = groceries.toArray(new String[groceries.size()]);
+        System.out.println(Arrays.toString(groceryArray));
     }
 
 }
