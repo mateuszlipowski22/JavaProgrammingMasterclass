f6b631532afa015d6900f65414948594ff28dbc9
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-08 01:17:28 +0100

    Section 10 - array list

---
+++ src/section10/array_list/Main.java
@@ -0,0 +1,31 @@
+package section10.array_list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem(String name, String type, int count){
+
+    public GroceryItem(String name){
+        this(name, "Dairy", 1);
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        GroceryItem[] groceryArray = new GroceryItem[3];
+        groceryArray[0]=new GroceryItem("milk");
+        groceryArray[1]=new GroceryItem("apples", "Produce", 6);
+        groceryArray[2] = new GroceryItem("oranges", "Produce", 5);
+        System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<GroceryItem>();
+        groceryList.add(new GroceryItem("Butter"));
+    }
+
+}
