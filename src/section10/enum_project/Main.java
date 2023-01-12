4ea644ab294117a723d28558b41a860f838f4fed
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-12 21:18:23 +0100

    Section 10 - enum #2

--- src/section10/enum_project/Main.java
+++ src/section10/enum_project/Main.java
@@ -11,10 +11,15 @@ public class Main {
 
         for (int i = 0; i < 10; i++) {
             weekDay=getRandom();
-            System.out.printf("Name is %s, Ordinal Value = %d%n", weekDay.name(), weekDay.ordinal());
-            if(weekDay==DayOfWeek.FRI){
-                System.out.println("Found a Friday");
-            }
+//            System.out.printf("Name is %s, Ordinal Value = %d%n", weekDay.name(), weekDay.ordinal());
+//            if(weekDay==DayOfWeek.FRI){
+//                System.out.println("Found a Friday");
+//            }
+            switchDayOfWeek(weekDay);
+        }
+
+        for(Topping topping : Topping.values()){
+            System.out.println(topping.name() + " : "+ topping.getPrice());
         }
 
     }
@@ -23,6 +28,15 @@ public class Main {
         int randomInteger = new Random().nextInt(7);
         var allDays = DayOfWeek.values();
         return allDays[randomInteger];
+    }
+
+    public static void switchDayOfWeek(DayOfWeek weekDay){
+        int weekDayInteger = weekDay.ordinal()+1;
+        switch (weekDay){
+            case WED -> System.out.println("Wednesday is Day " + weekDayInteger);
+            case SAT -> System.out.println("Saturday is Day " + weekDayInteger);
+            default -> System.out.println(weekDay.name().charAt(0)+weekDay.name().substring(1).toLowerCase()+"day is Day "+weekDayInteger);
+        }
     }
 
 }
