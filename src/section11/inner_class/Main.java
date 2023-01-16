a4a00832bf2770210721aaac187aa28680a4b920
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-16 01:39:11 +0100

    Section 11 - inner class

---
+++ src/section11/inner_class/Main.java
@@ -0,0 +1,14 @@
+package section11.inner_class;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Gearbox mcLaren = new Gearbox(6);
+        Gearbox.Gear first = mcLaren.new Gear(1,12.3);
+        System.out.println(first.driveSpeed(1000));
+
+
+    }
+
+}
