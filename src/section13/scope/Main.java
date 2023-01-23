2e3e77116d6d34e743581ce2bfa299101bc5dbc5
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-23 01:14:05 +0100

    Section 13 - Scope

---
+++ src/section13/scope/Main.java
@@ -0,0 +1,20 @@
+package section13.scope;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String privateVar = "this is private to main()";
+
+        ScopeCheck scopeInstance = new ScopeCheck();
+        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
+
+        System.out.println(privateVar);
+
+        scopeInstance.timesTwo();
+
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+        innerClass.timesTwo();
+    }
+
+}
