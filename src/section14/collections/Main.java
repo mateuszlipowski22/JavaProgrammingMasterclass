28f7207cb57486f96d990253a846716d565ba8ab
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-26 22:11:36 +0100

    Section 14 - comparable and comparator

--- src/section14/collections/Main.java
+++ src/section14/collections/Main.java
@@ -9,30 +9,46 @@ public class Main {
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Olimpian", 8, 12);
 
-        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
-        printList(seatCopy);
-        seatCopy.get(1).reserve();
+//        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
+//        printList(seatCopy);
+//        seatCopy.get(1).reserve();
 
-        if(theatre.reserveSeat("A02")){
-            System.out.println("Please pay for A02");
+        if(theatre.reserveSeat("D12")){
+            System.out.println("Please pay for D12");
         }else {
             System.out.println("Seat already taken");
         }
 
-        Collections.shuffle(seatCopy);
-        System.out.println("Printing seatCopy");
-        printList(seatCopy);
+        if(theatre.reserveSeat("D12")){
+            System.out.println("Please pay for D12");
+        }else {
+            System.out.println("Seat already taken");
+        }
 
-        System.out.println("Printing theatre.seat");
-        printList(theatre.seats);
+        if(theatre.reserveSeat("B13")){
+            System.out.println("Please pay for B13");
+        }else {
+            System.out.println("Seat already taken");
+        }
 
-        Theatre.Seat minSeat = Collections.min(seatCopy);
-        Theatre.Seat maxSeat = Collections.max(seatCopy);
-        System.out.println("Min seat number is "+ minSeat.getSeatNumber());
-        System.out.println("Max seat number is "+ maxSeat.getSeatNumber());
+        List<Theatre.Seat> reverseSeats = new ArrayList<>(theatre.getSeats());
+        Collections.reverse(reverseSeats);
+        printList(reverseSeats);
 
-        sortList(seatCopy);
-        printList(seatCopy);
+//        Collections.shuffle(seatCopy);
+//        System.out.println("Printing seatCopy");
+//        printList(seatCopy);
+//
+//        System.out.println("Printing theatre.seat");
+//        printList(theatre.seats);
+//
+//        Theatre.Seat minSeat = Collections.min(seatCopy);
+//        Theatre.Seat maxSeat = Collections.max(seatCopy);
+//        System.out.println("Min seat number is "+ minSeat.getSeatNumber());
+//        System.out.println("Max seat number is "+ maxSeat.getSeatNumber());
+//
+//        sortList(seatCopy);
+//        printList(seatCopy);
 
 //        List<Theatre.Seat> newList = new ArrayList<>();
 //        Collections.copy(newList, theatre.seats);
@@ -43,24 +59,28 @@ public class Main {
 //        }else {
 //            System.out.println("Sorry the seat is taken");
 //        }
+
+        List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
+        priceSeats.add(theatre.new Seat("B00", 13.00));
+        priceSeats.add(theatre.new Seat("A00", 13.00));
+        Collections.sort(priceSeats,Theatre.PRICE_ORDER);
+        printList(priceSeats);
+
     }
     public static void printList(List<Theatre.Seat> list){
         for (Theatre.Seat seat : list){
-            System.out.print(" "+seat.getSeatNumber());
+            System.out.print(" "+seat.getSeatNumber()+" "+seat.getPrice());
         }
         System.out.println("=================");
     }
 
-    public static void sortList(List<? extends Theatre.Seat> list){
-        for (int i=0; i<list.size()-1; i++){
-            for (int j=i+1; j<list.size(); j++){
-                if(list.get(i).compareTo(list.get(j))>0){
-                    Collections.swap(list, i, j);
-                }
-            }
-        }
-    }
-
-
-
+//    public static void sortList(List<? extends Theatre.Seat> list){
+//        for (int i=0; i<list.size()-1; i++){
+//            for (int j=i+1; j<list.size(); j++){
+//                if(list.get(i).compareTo(list.get(j))>0){
+//                    Collections.swap(list, i, j);
+//                }
+//            }
+//        }
+//    }
 }
