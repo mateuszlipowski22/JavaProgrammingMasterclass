c111dd9ba78943bc02a2724ec553f9e3db675776
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-27 01:22:16 +0100

    Section 14 - adventure game

--- src/section14/collections/MapProgram.java
+++ src/section14/collections/MapProgram.java
@@ -30,6 +30,21 @@ public class MapProgram {
 //        System.out.println(languages.get("Java"));
 
         System.out.println("=========================================================================================================");
+
+//        languages.remove("Lisp");
+        if(languages.remove("Algol", "a family of algorithmic language")){
+            System.out.println("Algol removed");
+        }else {
+            System.out.println("Algol not removed, key/value pair not found");
+        }
+
+        if(languages.replace("Lisp","this will not work", "a functional programming language with imperative features")){
+            System.out.println("Lisp replaced");
+        }else {
+            System.out.println("Lisp was not replaced");
+        }
+        System.out.println(languages.replace("Scala", "this will not be added"));
+
         for (String key : languages.keySet()){
             System.out.println(key + " : "+languages.get(key));
         }
