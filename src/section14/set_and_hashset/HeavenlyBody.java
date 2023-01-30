c2b7ea39662ce2b642c516ee9b2e7da54db90214
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-30 22:04:45 +0100

    Section 14 - equals

--- src/section14/set_and_hashset/HeavenlyBody.java
+++ src/section14/set_and_hashset/HeavenlyBody.java
@@ -30,4 +30,20 @@ public final class HeavenlyBody {
     public Set<HeavenlyBody> getSattellites() {
         return new HashSet<>(this.sattellites);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this==obj){
+            return true;
+        }
+
+        System.out.println("obj.getClass() is "+ obj.getClass());
+        System.out.println("this.getClass() is "+ this.getClass());
+        if(obj==null || obj.getClass()!=this.getClass()){
+            return false;
+        }
+
+        String objName = ((HeavenlyBody) obj).getName();
+        return this.name.equals(objName);
+    }
 }
