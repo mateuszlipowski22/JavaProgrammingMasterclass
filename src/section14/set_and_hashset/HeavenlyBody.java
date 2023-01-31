62bdc31ab61503a2f16e528db691df9378fdeb9b
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-31 02:21:11 +0100

    Section 14 - set challenge #3

--- src/section14/set_and_hashset/HeavenlyBody.java
+++ src/section14/set_and_hashset/HeavenlyBody.java
@@ -5,10 +5,10 @@ import java.util.Set;
 
 public abstract class HeavenlyBody {
 
-    private final String name;
+    private final Key key;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> sattellites;
-    private final BodyTypes bodyType;
+
 
     public enum BodyTypes {
         STAR,
@@ -20,25 +20,20 @@ public abstract class HeavenlyBody {
     }
 
     public HeavenlyBody(String name, double orbitalPeriod, BodyTypes bodyType) {
-        this.name = name;
+        this.key = new Key(name, bodyType);
         this.orbitalPeriod = orbitalPeriod;
         this.sattellites = new HashSet<>();
-        this.bodyType = bodyType;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public double getOrbitalPeriod() {
         return orbitalPeriod;
     }
 
-    public BodyTypes getBodyType() {
-        return bodyType;
+    public Key getKey() {
+        return key;
     }
 
-    public boolean addSatelite(HeavenlyBody moon){
+    public boolean addSatelite(HeavenlyBody moon) {
         return this.sattellites.add(moon);
     }
 
@@ -48,27 +43,67 @@ public abstract class HeavenlyBody {
 
     @Override
     public final boolean equals(Object obj) {
-        if(this==obj){
+        if (this == obj) {
             return true;
         }
 
-        if(obj instanceof HeavenlyBody){
+        if (obj instanceof HeavenlyBody) {
             HeavenlyBody theObject = (HeavenlyBody) obj;
-            if(this.name.equals(theObject.getName())){
-                return this.bodyType == theObject.getBodyType();
-            }
+            return this.key.equals(theObject.getKey());
         }
         return false;
     }
 
     @Override
     public final int hashCode() {
-        System.out.println("hashCode is called");
-        return this.name.hashCode()+57+this.bodyType.hashCode();
+//        System.out.println("hashCode is called");
+        return this.key.hashCode();
     }
 
     @Override
     public String toString() {
-        return this.name + " : " + this.bodyType + ", " + this.orbitalPeriod;
+        return this.key.name + " : " + this.key.bodyType + ", " + this.orbitalPeriod;
+    }
+
+    public static final class Key {
+        private String name;
+        private BodyTypes bodyType;
+
+        public Key(String name, BodyTypes bodyTypes) {
+            this.name = name;
+            this.bodyType = bodyTypes;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public BodyTypes getBodyType() {
+            return bodyType;
+        }
+
+        public static Key makeKey(String name, BodyTypes bodyTypes) {
+            return new Key(name, bodyTypes);
+        }
+
+        @Override
+        public int hashCode() {
+            return this.name.hashCode() + 57 + this.bodyType.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Key key = (Key) obj;
+
+            if (this.name.equals(key.getName())) {
+                return this.bodyType == ((Key) obj).getBodyType();
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return this.name + " : " + this.bodyType;
+        }
     }
 }
--- src/section14/set_and_hashset/Main.java
+++ src/section14/set_and_hashset/Main.java
@@ -6,83 +6,83 @@ import java.util.Map;
 import java.util.Set;
 
 public class Main {
-    private static Map<String, HeavenlyBody> solarSystem=new HashMap<>();
+    private static Map<HeavenlyBody.Key, HeavenlyBody> solarSystem=new HashMap<>();
     private static Set<HeavenlyBody> planets = new HashSet<>();
 
     public static void main(String[] args) {
         HeavenlyBody temp = new Planet("Mercury", 88);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Venus", 225);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Earth", 365);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         HeavenlyBody tempMoon = new Moon("Moon", 27);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatelite(tempMoon);
 
         temp = new Planet("Mars", 687);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         tempMoon = new Moon("Deimos", 1.3);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatelite(tempMoon); // temp is still Mars
 
         tempMoon = new Moon("Phobos", 0.3);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatelite(tempMoon); // temp is still Mars
 
         temp = new Planet("Jupiter", 4332);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         tempMoon = new Moon("Io", 1.8);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatelite(tempMoon); // temp is still Jupiter
 
         tempMoon = new Moon("Europa", 3.5);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatelite(tempMoon); // temp is still Jupiter
 
         tempMoon = new Moon("Ganymede", 7.1);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatelite(tempMoon); // temp is still Jupiter
 
         tempMoon = new Moon("Callisto", 16.7);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatelite(tempMoon); // temp is still Jupiter
 
         temp = new Planet("Saturn", 10759);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Uranus", 30660);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Neptune", 165);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Pluto", 248);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         System.out.println("Planets");
         for (HeavenlyBody planet : planets) {
-            System.out.println("\t"+planet.getName());
+            System.out.println("\t"+planet.getKey().getName());
         }
 
-        HeavenlyBody body = solarSystem.get("Mars");
-        System.out.println("Moons of " + body.getName());
+        HeavenlyBody body = solarSystem.get(HeavenlyBody.Key.makeKey("Mars", HeavenlyBody.BodyTypes.PLANET));
+        System.out.println("Moons of " + body.getKey().getName());
         for (HeavenlyBody sattellite : body.getSattellites()) {
-            System.out.println("\t"+sattellite.getName());
+            System.out.println("\t"+sattellite.getKey().getName());
         }
 
         Set<HeavenlyBody> moons = new HashSet<>();
@@ -92,7 +92,7 @@ public class Main {
 
         System.out.println("All Moons");
         for (HeavenlyBody moon : moons) {
-            System.out.println("\t"+moon.getName());
+            System.out.println("\t"+moon.getKey().getName());
         }
 
         HeavenlyBody pluto = new DwarfPlanet("Pluto", 842);
@@ -109,5 +109,16 @@ public class Main {
         System.out.println("earth2.equals(earth1) = " + earth2.equals(earth1));
         System.out.println("earth1.equals(pluto) = " + earth1.equals(pluto));
         System.out.println("pluto.equals(earth2) = " + pluto.equals(earth2));
+
+        solarSystem.put(pluto.getKey(), pluto);
+        System.out.println(solarSystem.get(HeavenlyBody.Key.makeKey("Pluto", HeavenlyBody.BodyTypes.PLANET)));
+        System.out.println(solarSystem.get(HeavenlyBody.Key.makeKey("Pluto", HeavenlyBody.BodyTypes.DWARF_PLANET)));
+
+
+        System.out.println();
+        System.out.println("The solar system cointains of");
+        for (HeavenlyBody heavenlyBody : solarSystem.values()) {
+            System.out.println(heavenlyBody);
+        }
     }
 }
