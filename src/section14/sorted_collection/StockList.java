35018098e8d1f3b2925aaeb9f3a244ce7269f354
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-01 00:40:16 +0100

    Section 14 - adding basket

--- src/section14/sorted_collection/StockList.java
+++ src/section14/sorted_collection/StockList.java
@@ -1,15 +1,15 @@
 package section14.sorted_collection;
 
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class StockList {
 
     private final Map<String, StockItem> list;
 
-    public StockList(Map<String, StockItem> list) {
-        this.list = new HashMap<>();
+    public StockList() {
+        this.list = new LinkedHashMap<>();
     }
 
     public int addStock(StockItem item){
@@ -56,7 +56,7 @@ public class StockList {
 
             double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
             s = s + stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
-            s = s + itemValue + "\n";
+            s = s + String.format("%.2f",itemValue) + "\n";
             totalCost+=itemValue;
         }
 
