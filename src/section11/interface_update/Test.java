500055c4d8fff7b11388c971b7e96e648f0ceac5
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-12 01:11:40 +0100

    Section 11 - updated interface public static and private methods

--- src/section11/interface_update/Test.java
+++ src/section11/interface_update/Test.java
@@ -6,6 +6,8 @@ public class Test {
 
         inFlight(new Jet());
 
+//        OrbitEarth.log("Testing "+new Sattelite());
+        orbit(new Sattelite());
     }
 
     private static void inFlight(FlightEnabled flier){
@@ -16,6 +18,12 @@ public class Test {
         if(flier instanceof Trackable tracked){
             tracked.track();
         }
+        flier.land();
+    }
+
+    private static void orbit(OrbitEarth flier){
+        flier.takeOff();
+        flier.fly();
         flier.land();
     }
 }
