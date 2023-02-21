271df9fe2ed7b7ed0c9920cc5a6ff07154064d19
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-22 00:55:05 +0100

    Section 16 - Catching and throwing exceptions

--- src/section15/exceptions/Example.java
+++ src/section15/exceptions/Example.java
@@ -16,12 +16,10 @@ public class Example {
         try{
             x = getInt();
             y = getInt();
+            System.out.println("x is " +x +", y is " + y);
+            return x/y;
         }catch (NoSuchElementException e){
             throw new ArithmeticException("No suitable input");
-        }
-        System.out.println("x is " +x +", y is " + y);
-        try{
-            return x/y;
         }catch (ArithmeticException e){
             throw new ArithmeticException("attempt to divide by zero");
         }
