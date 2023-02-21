674578b05cc74573a4d0fccaca6e7e1a717c1b84
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-22 00:26:29 +0100

    Section 16 - Exceptions

---
+++ src/section15/exceptions/Main.java
@@ -0,0 +1,72 @@
+package section15.exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        int x = 98;
+//        int y = 0;
+//        System.out.println(dividedLBYL(x, y));
+//        System.out.println(divideEAFP(x, y));
+//        System.out.println(divide(x, y));
+        int x = getIntEAFP();
+        System.out.println("x = "+x);
+    }
+
+    private static int getInt(){
+        Scanner s =new Scanner(System.in);
+        return s.nextInt();
+    }
+
+    private static int getIntLBYL(){
+        Scanner s = new Scanner(System.in);
+        boolean isValid = true;
+        System.out.println("Please enter an integer");
+        String input = s.next();
+        for(int i=0; i<input.length(); i++){
+            if(!Character.isDigit(input.charAt(i))){
+                isValid=false;
+                break;
+            }
+        }
+        if(isValid){
+            return  Integer.parseInt(input);
+        }
+        return 0;
+    }
+
+    private static int getIntEAFP(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an integer");
+        try{
+            return scanner.nextInt();
+        }catch (InputMismatchException e){
+            return 0;
+        }
+    }
+
+    private static int dividedLBYL(int x, int y){
+        if(y!=0){
+            return x/y;
+        }else{
+            return 0;
+        }
+    }
+
+    private static int divideEAFP(int x, int y){
+        try {
+            return x/y;
+        }catch (ArithmeticException e){
+            return 0;
+        }
+    }
+
+    private static int divide(int x, int y){
+
+        return x/y;
+
+    }
+}
