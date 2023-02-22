af31e867f7f6379d3094005d7b6f1aa3e594811d
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-22 01:02:03 +0100

    Section 16 - Multi catch excpetion

--- src/section15/exceptions/Example.java
+++ src/section15/exceptions/Example.java
@@ -7,22 +7,28 @@ import java.util.Scanner;
 public class Example {
 
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+        try{
+            int result = divide();
+            System.out.println(result);
+        }catch (ArithmeticException | NoSuchElementException e){
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division, shutting down");
+        }
+
     }
 
     private static int divide(){
         int x, y;
-        try{
+//        try{
             x = getInt();
             y = getInt();
             System.out.println("x is " +x +", y is " + y);
             return x/y;
-        }catch (NoSuchElementException e){
-            throw new ArithmeticException("No suitable input");
-        }catch (ArithmeticException e){
-            throw new ArithmeticException("attempt to divide by zero");
-        }
+//        }catch (NoSuchElementException e){
+//            throw new ArithmeticException("No suitable input");
+//        }catch (ArithmeticException e){
+//            throw new ArithmeticException("attempt to divide by zero");
+//        }
     }
 
     private static int getInt(){
