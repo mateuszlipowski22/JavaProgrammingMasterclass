e180a6a7c5a501fe0feea9c28279e300b65a9fa6
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-24 21:50:53 +0100

    Section 16 - byte streams

--- src/section16/inputOutput/Locations.java
+++ src/section16/inputOutput/Locations.java
@@ -9,20 +9,23 @@ public class Locations implements Map<Integer, Location> {
 
     public static void main(String[] args) throws IOException {
 
-
-        try (FileWriter locFile = new FileWriter("locations.txt");
-             FileWriter dirFile = new FileWriter("directions.txt")
-        ) {
-            for (Location location : locations.values()) {
-                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
-                for (String direction : location.getExits().keySet()) {
+        try (DataOutputStream locFile = new DataOutputStream(new BufferedOutputStream(new FileOutputStream( "location.dat")))) {
+            for(Location location : locations.values()){
+                locFile.writeInt(location.getLocationID());
+                locFile.writeUTF(location.getDescription());
+                System.out.println("Writing location " + location.getLocationID() + " : " + location.getDescription());
+                System.out.println("Writing " + (location.getExits().size()-1) + " exits." );
+                locFile.writeInt(location.getExits().size()-1);
+                for (String direction : location.getExits().keySet()){
                     if(!direction.equalsIgnoreCase("Q")){
-                        dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
+                        System.out.println("\t\t" + direction + "," + location.getExits().get(direction));
+                        locFile.writeUTF(direction);
+                        locFile.writeInt(location.getExits().get(direction));
+
                     }
                 }
             }
         }
-
     }
 
     static {
