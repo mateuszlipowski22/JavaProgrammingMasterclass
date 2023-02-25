f93c99b1953010aff72b4c8823e3395ccf0202f6
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-26 00:30:24 +0100

    Section 16 - Writing binary files with Java NIO

--- src/section16/javaNIO/Main.java
+++ src/section16/javaNIO/Main.java
@@ -1,27 +1,44 @@
 package section16.javaNIO;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        try{
+        try(FileOutputStream binFile = new FileOutputStream("data.dat");
+            FileChannel binChannel = binFile.getChannel()){
+
+            byte[] outputByte = "Hello World".getBytes(StandardCharsets.UTF_8);
+            ByteBuffer buffer = ByteBuffer.wrap(outputByte);
+            int numBytes = binChannel.write(buffer);
+            System.out.println("numBytes written was: "+numBytes);
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(245);
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("numBytes written was: "+numBytes);
+
+            intBuffer.flip();
+            intBuffer.putInt(-232245);
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("numBytes written was: "+numBytes);
+
 //            FileInputStream file = new FileInputStream("data.txt");
 //            FileChannel channel = file.getChannel();
 
-            Path dataPath = FileSystems.getDefault().getPath("data.txt");
-            Files.write(dataPath,"\nLine 5".getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
-            List<String> lines = Files.readAllLines(dataPath);
-            for(String line : lines){
-                System.out.println(line);
-            }
+//            Path dataPath = FileSystems.getDefault().getPath("data.txt");
+//            Files.write(dataPath,"\nLine 5".getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+//            List<String> lines = Files.readAllLines(dataPath);
+//            for(String line : lines){
+//                System.out.println(line);
+//            }
 
         } catch (IOException e) {
             e.printStackTrace();
