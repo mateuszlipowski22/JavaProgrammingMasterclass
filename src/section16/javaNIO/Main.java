4a9241b7906dcc4ecc9fde4ee6deef16e966bd12
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-03 22:33:37 +0100

    Section 16 - writing sequentially

--- src/section16/javaNIO/Main.java
+++ src/section16/javaNIO/Main.java
@@ -37,18 +37,67 @@ public class Main {
 
             RandomAccessFile ra = new RandomAccessFile("data.dat","rwd");
             FileChannel channel = ra.getChannel();
-            ByteBuffer readChannel = ByteBuffer.allocate(100);
-            channel.read(readChannel);
-            readChannel.flip();
-            byte[] inputString = new byte[outputByte.length];
-            readChannel.get(inputString);
-            System.out.println("inputString ="+new String(inputString));
-            System.out.println("int1 = "+readChannel.getInt());
-            System.out.println("int2 = "+readChannel.getInt());
-            byte[] inputString2 = new byte[outputByte2.length];
-            readChannel.get(inputString2);
-            System.out.println("inputString2 ="+new String(inputString2));
-            System.out.println("int3 = "+readChannel.getInt());
+
+            ByteBuffer readBuffer = ByteBuffer.allocate(Integer.BYTES);
+            channel.position(int3Pos);
+            channel.read(readBuffer);
+            readBuffer.flip();
+            System.out.println("int3 = "+readBuffer.getInt());
+
+            readBuffer.flip();
+            channel.position(int2Pos);
+            channel.read(readBuffer);
+            readBuffer.flip();
+            System.out.println("int2 = "+readBuffer.getInt());
+
+            channel.position(int1Pos);
+            channel.read(readBuffer);
+            readBuffer.flip();
+            System.out.println("int1 = "+readBuffer.getInt());
+
+            byte[] outputString = "Hello, world!".getBytes();
+            long str1Pos = 0;
+            long newInt1Pos = outputString.length;
+            long newInt2Pos = newInt1Pos + Integer.BYTES;
+            byte[] outputString2 = "Nice to meet you".getBytes();
+            long str2Pos = newInt2Pos+Integer.BYTES;
+            long newInt3Pos = str2Pos+outputString2.length;
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(245);
+            intBuffer.flip();
+            binChannel.position(newInt1Pos);
+            binChannel.write(intBuffer);
+
+            intBuffer.flip();
+            intBuffer.putInt(32323);
+            intBuffer.flip();
+            binChannel.position(newInt2Pos);
+            binChannel.write(intBuffer);
+
+            intBuffer.flip();
+            intBuffer.putInt(1000);
+            intBuffer.flip();
+            binChannel.position(newInt3Pos);
+            binChannel.write(intBuffer);
+
+            binChannel.position(str1Pos);
+            binChannel.write(ByteBuffer.wrap(outputString));
+            binChannel.position(str2Pos);
+            binChannel.write(ByteBuffer.wrap(outputString2));
+
+//            ByteBuffer readChannel = ByteBuffer.allocate(100);
+//            channel.read(readChannel);
+//            readChannel.flip();
+//            byte[] inputString = new byte[outputByte.length];
+//            readChannel.get(inputString);
+//            System.out.println("inputString ="+new String(inputString));
+//            System.out.println("int1 = "+readChannel.getInt());
+//            System.out.println("int2 = "+readChannel.getInt());
+//            byte[] inputString2 = new byte[outputByte2.length];
+//            readChannel.get(inputString2);
+//            System.out.println("inputString2 ="+new String(inputString2));
+//            System.out.println("int3 = "+readChannel.getInt());
 
 
 
