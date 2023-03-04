8029ff6b0d6e6e845d2df28fc910f456c394436e
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-04 22:06:38 +0100

    Section 16 - More on Paths

--- src/section16/paths/Main.java
+++ src/section16/paths/Main.java
@@ -10,11 +10,19 @@ public class Main {
 
         Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
         printFile(path);
-        Path filePath = FileSystems.getDefault().getPath("files","SubDirectoryFile.txt");
+//        Path filePath = FileSystems.getDefault().getPath("files","SubDirectoryFile.txt");
+        Path filePath = Paths.get(".","files","SubDirectoryFile.txt");
         printFile(filePath);
-        Path outerFilePath = Paths.get("/home/mateusz/Programowanie - Mateusz/Udemy/Java Programming Masterclass/OutDirectory.txt");
+//        Path outerFilePath = Paths.get("/home/mateusz/Programowanie - Mateusz/Udemy/Java Programming Masterclass/OutDirectory.txt");
+        Path outerFilePath = Paths.get("/home/mateusz/Programowanie - Mateusz/Udemy","/Java Programming Masterclass/","OutDirectory.txt");
         printFile(outerFilePath);
 
+        filePath = Paths.get(".");
+        System.out.println(filePath.toAbsolutePath());
+
+        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", "SubDirectoryFile.txt");
+        printFile(path2);
+        System.out.println(path2.normalize().toAbsolutePath());
     }
 
     public static void printFile(Path path){
