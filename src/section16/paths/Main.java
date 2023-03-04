d139af4641ed2afaf0fd82f135e67c555c7aa945
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-04 22:29:24 +0100

    Section 16 - Exists and CopyFile

--- src/section16/paths/Main.java
+++ src/section16/paths/Main.java
@@ -8,21 +8,41 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
-        printFile(path);
-//        Path filePath = FileSystems.getDefault().getPath("files","SubDirectoryFile.txt");
-        Path filePath = Paths.get(".","files","SubDirectoryFile.txt");
-        printFile(filePath);
-//        Path outerFilePath = Paths.get("/home/mateusz/Programowanie - Mateusz/Udemy/Java Programming Masterclass/OutDirectory.txt");
-        Path outerFilePath = Paths.get("/home/mateusz/Programowanie - Mateusz/Udemy","/Java Programming Masterclass/","OutDirectory.txt");
-        printFile(outerFilePath);
+        try{
+            Path sourceFile = FileSystems.getDefault().getPath("Example","file1.txt");
+            Path copyFile = FileSystems.getDefault().getPath("Example","file1copy.txt");
+            Files.copy(sourceFile, copyFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        filePath = Paths.get(".");
-        System.out.println(filePath.toAbsolutePath());
 
-        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", "SubDirectoryFile.txt");
-        printFile(path2);
-        System.out.println(path2.normalize().toAbsolutePath());
+//        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+//        printFile(path);
+////        Path filePath = FileSystems.getDefault().getPath("files","SubDirectoryFile.txt");
+//        Path filePath = Paths.get(".","files","SubDirectoryFile.txt");
+//        printFile(filePath);
+////        Path outerFilePath = Paths.get("/home/mateusz/Programowanie - Mateusz/Udemy/Java Programming Masterclass/OutDirectory.txt");
+//        Path outerFilePath = Paths.get("/home/mateusz/Programowanie - Mateusz/Udemy","/Java Programming Masterclass/","OutDirectory.txt");
+//        printFile(outerFilePath);
+//
+//        filePath = Paths.get(".");
+//        System.out.println(filePath.toAbsolutePath());
+//
+//        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", "SubDirectoryFile.txt");
+//        printFile(path2);
+//        System.out.println(path2.normalize().toAbsolutePath());
+//
+//        Path path3 = FileSystems.getDefault().getPath("thisFileDoesNotExist.txt" );
+//        System.out.println(path3.toAbsolutePath());
+//
+//        Path path4 = Paths.get("/volumes/test/ing","abcdef","no.txt");
+//        System.out.println(path4.toAbsolutePath());
+//
+//        filePath = FileSystems.getDefault().getPath("files");
+//        System.out.println("Exists = "+Files.exists(filePath));
+//        System.out.println("Exists = "+Files.exists(path4));
+
     }
 
     public static void printFile(Path path){
