dc0011b8767dd0d72d622c7c9e0e65155ff19cd2
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-08 01:36:24 +0100

    Section 17 - Thread pool

--- src/section17/producerConsumer/Main.java
+++ src/section17/producerConsumer/Main.java
@@ -3,6 +3,7 @@ package section17.producerConsumer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 import static section17.producerConsumer.Main.EOF;
@@ -13,13 +14,32 @@ public class Main {
     public static void main(String[] args) {
         List<String> buffer = new ArrayList<>();
         ReentrantLock bufferLock = new ReentrantLock();
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
         MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_BLUE, bufferLock);
         MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_PURPLE, bufferLock);
         MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_RED, bufferLock);
 
-        new Thread(producer).start();
-        new Thread(consumer1).start();
-        new Thread(consumer2).start();
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2);
+
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(ThreadColor.ANSI_RED+"I am being printed for Callable class");
+                return "This is callable result";
+            }
+        });
+
+        try {
+            System.out.println(future.get());
+        } catch (ExecutionException e) {
+            System.out.println("Something went wrong");
+        } catch (InterruptedException e) {
+            System.out.println("Thread running the task was interrupted");
+        }
+
+        executorService.shutdown();
 
     }
 
