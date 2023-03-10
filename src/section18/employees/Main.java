b587ce1691b9e2489d073d34a5d65c50d5e9b946
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-10 22:49:54 +0100

    Section 18 - Functional interface and predicates

---
+++ src/section18/employees/Main.java
@@ -0,0 +1,56 @@
+package section18.employees;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee john = new Employee("John", 30);
+        Employee tim = new Employee("Tim", 21);
+        Employee jack = new Employee("Jack", 40);
+        Employee snow = new Employee("Snow White", 33);
+        Employee red = new Employee("Red", 40);
+        Employee charming = new Employee("Prince Charming", 33);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(jack);
+        employees.add(john);
+        employees.add(tim);
+        employees.add(snow);
+        employees.add(red);
+        employees.add(charming);
+
+//        System.out.println("Employees 30 and younger");
+//        System.out.println("=================");
+//        employees.forEach(employee -> {
+//            if (employee.getAge() <= 30) {
+//                System.out.println(employee.getName());
+//            }
+//        });
+
+//        for (Employee employee : employees) {
+//            if(employee.getAge()>30){
+//                System.out.println(employee.getName());
+//            }
+//        }
+
+        printEmployeeByAge(employees, "Employees over 30", employee -> employee.getAge()>30);
+        printEmployeeByAge(employees, "Employees 30 and younger", employee -> employee.getAge()<=30);
+
+    }
+
+    private static void printEmployeeByAge(List<Employee> employees,
+                                           String ageText,
+                                           Predicate<Employee> ageCondition){
+        System.out.println(ageText);
+        System.out.println("=================");
+        for (Employee employee : employees) {
+            if (ageCondition.test(employee)){
+                System.out.println(employee.getName());
+            }
+        }
+    }
+}
