dba316dc3e4f915f4508c768a13f76d911e171bf
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-13 22:26:18 +0100

    Section 19 - and, or & not

--- src/section19/regular_expressions/Main.java
+++ src/section19/regular_expressions/Main.java
@@ -95,7 +95,45 @@ public class Main {
         while(h2TextMatcher.find()){
             System.out.println("Occurrence "+h2TextMatcher.group(2));
         }
+        // [Hh]arry
+        System.out.println("harry".replaceAll("[H|h]arry","Larry"));
+        System.out.println("Harry".replaceAll("[H|h]arry","Larry"));
 
+        // [^abc]
+        String tvTest = "tstvtkt";
+        String tNotVRegExp = "t(?!v)"; //"t[^v]";
+        Pattern tNotVPattern = Pattern.compile(tNotVRegExp);
+        Matcher tNotVMatcher = tNotVPattern.matcher(tvTest);
+
+        count = 0;
+        while(tNotVMatcher.find()){
+            count++;
+            System.out.println("Occurrence "+count+" : "+tNotVMatcher.start()+" to "+tNotVMatcher.end());
+        }
+
+        // ^([\(]{1}[0-9]{3}[\)]{1}[ ]{1}[0-9]{3}[\-]{1}[0-9]{4})$
+        String telephoneRegExp = "^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$";
+        String phone1 = "1234567890";
+        String phone2 = "(123) 456-7890";
+        String phone3 = "123 456-7890";
+        String phone4 = "(123)456-7890";
+
+        System.out.println("phone1 = "+phone1.matches(telephoneRegExp));
+        System.out.println("phone2 = "+phone2.matches(telephoneRegExp));
+        System.out.println("phone3 = "+phone3.matches(telephoneRegExp));
+        System.out.println("phone4 = "+phone4.matches(telephoneRegExp));
+
+        // ^4[0-9]{12}([0-9]{3})?$
+        String visaRegExp = "^4[0-9]{12}([0-9]{3})?$";
+        String visa1 = "4444444444444";
+        String visa2 = "5444444444444";
+        String visa3 = "44444444444444444";
+        String visa4 = "4444";
+
+        System.out.println("visa1 = "+visa1.matches(visaRegExp));
+        System.out.println("visa2 = "+visa2.matches(visaRegExp));
+        System.out.println("visa3 = "+visa3.matches(visaRegExp));
+        System.out.println("visa4 = "+visa4.matches(visaRegExp));
 
     }
 
