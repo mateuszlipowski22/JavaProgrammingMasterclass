f9abcbe4b9bc18fd6b092dbafc620e0acfa5c77f
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-15 01:46:08 +0100

    Section 20 - asserts in junit

--- src/section20/banking/BankAccount.java
+++ src/section20/banking/BankAccount.java
@@ -5,11 +5,16 @@ public class BankAccount {
     private String firstName;
     private String lastName;
     private double balance;
+    public static final int CHECKING = 1;
+    public static final int SAVINGS = 2;
 
-    public BankAccount(String firstName, String lastName, double balance) {
+    private int accountType;
+
+    public BankAccount(String firstName, String lastName, double balance, int typeAccount) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = balance;
+        this.accountType=typeAccount;
     }
 
     public double deposit(double amount, boolean branch){
@@ -24,5 +29,9 @@ public class BankAccount {
 
     public double getBalance() {
         return balance;
+    }
+
+    public boolean isChecking() {
+        return accountType==CHECKING;
     }
 }
--- src/section20/banking/BankAccountTest.java
+++ src/section20/banking/BankAccountTest.java
@@ -1,12 +1,16 @@
 package section20.banking;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankAccountTest {
 
     @org.junit.jupiter.api.Test
     void deposit() {
-        fail("This test has yet to be implemented");
+        BankAccount bankAccount = new BankAccount("Tim", "John", 1000.00, BankAccount.CHECKING);
+        double balance = bankAccount.deposit(200, true);
+        assertEquals(1200, balance,0);
     }
 
     @org.junit.jupiter.api.Test
@@ -15,9 +19,23 @@ class BankAccountTest {
     }
 
     @org.junit.jupiter.api.Test
-    void getBalance() {
-        fail("This test has yet to be implemented");
+    void getBalance_deposite() {
+        BankAccount bankAccount = new BankAccount("Tim", "John", 1000.00, BankAccount.CHECKING);
+        double balance = bankAccount.deposit(200, true);
+        assertEquals(1200, bankAccount.getBalance(),0);
     }
 
+    @org.junit.jupiter.api.Test
+    void getBalance_withdraw() {
+        BankAccount bankAccount = new BankAccount("Tim", "John", 1000.00, BankAccount.CHECKING);
+        double balance = bankAccount.withdraw(200, true);
+        assertEquals(800, bankAccount.getBalance(),0);
+    }
+
+    @Test
+    public void isChecking_true(){
+        BankAccount bankAccount = new BankAccount("Tim", "John", 1000.00, BankAccount.SAVINGS);
+        assertTrue(bankAccount.isChecking(), "The account is NOT a checking account");
+    }
 }
 
