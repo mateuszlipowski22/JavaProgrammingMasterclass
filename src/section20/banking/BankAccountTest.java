2dc4d3c8d0968edf19f99cbf623baddff05212b8
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-15 01:16:29 +0100

    Section 20 - introduction to Junit

---
+++ src/section20/banking/BankAccountTest.java
@@ -0,0 +1,23 @@
+package section20.banking;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+        fail("This test has yet to be implemented");
+    }
+
+}
+
---
+++ src/section20/banking/Main.java
@@ -0,0 +1,8 @@
+package section20.banking;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("The application is running");
+    }
+}
