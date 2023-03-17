9f1ab9ed600897f0b6a404410c202efd10d9abf5
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-17 12:18:00 +0100

    Section 22 - JDBC insert, delete and update

--- src/section22/database/sqlite/Main.java
+++ src/section22/database/sqlite/Main.java
@@ -1,16 +1,28 @@
 package section22.database.sqlite;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
         try(Connection connection = DriverManager.getConnection("jdbc:sqlite:/home/mateusz/Programowanie - Mateusz/Udemy/Java Programming Masterclass/sqlite/testjava.db");
             Statement statement = connection.createStatement()){
-            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+//            connection.setAutoCommit(false);
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts "+
+                    "(name TEXT, phone INTEGER, email TEXT)");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Mateusz', 15648923,'mateusz@wp.pl')");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Jane', 15622223,'jane@wp.pl')");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Bob', 33348923,'bob@wp.pl')");
+//            statement.execute("UPDATE contacts SET phone=111111111 WHERE name='Jane'");
+//            statement.execute("DELETE FROM contacts WHERE name='Jane'");
+
+            statement.execute("SELECT * FROM 'contacts'");
+            ResultSet resultSet=statement.getResultSet();
+            while(resultSet.next()){
+                System.out.printf("| %15s | %20d | %20s |\n",resultSet.getString("name"),resultSet.getInt("phone"),resultSet.getString("email"));
+            }
+            resultSet.close();
+
         }catch (SQLException e){
             System.out.println("Something went wrong : "+e.getMessage());
         }
