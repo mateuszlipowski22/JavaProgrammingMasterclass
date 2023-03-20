bf3cadd02977aef0b747150f0988de570d97c155
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-20 22:00:21 +0100

    Section 22 - functions and views

--- src/section22/database/music/model/Datasource.java
+++ src/section22/database/music/model/Datasource.java
@@ -60,6 +60,22 @@ public class Datasource {
             " ORDER BY " + TABLE_ARTISTS + "." + COLUMN_ARTIST_NAME + ", " +
                     TABLE_ALBUMS + "." + COLUMN_ALBUM_NAME + " COLLATE NOCASE ";
 
+    public static final String TABLE_ARTIST_SONG_VIEW = "artist_list";
+    public static final String CREATE_ARTIST_FOR_SONG_VIEW = "CREATE VIEW IF NOT EXISTS "+
+            TABLE_ARTIST_SONG_VIEW+" AS SELECT "+TABLE_ARTISTS+"."+COLUMN_ARTIST_NAME+ ", "+
+            TABLE_ALBUMS+"."+COLUMN_ALBUM_NAME +" AS "+COLUMN_SONG_ALBUM+", "+
+            TABLE_SONGS+"."+COLUMN_SONG_TRACK +", "+TABLE_SONGS+"."+COLUMN_SONG_TITLE +
+            " FROM "+ TABLE_SONGS+
+            " INNER JOIN "+ TABLE_ALBUMS +" ON "+TABLE_SONGS+
+            "."+COLUMN_SONG_ALBUM+" = "+TABLE_ALBUMS+"."+COLUMN_ARTIST_ID+
+            " INNER JOIN "+ TABLE_ARTISTS +" ON "+TABLE_ALBUMS+
+            "."+COLUMN_ALBUM_ARTIST+" = "+TABLE_ARTISTS+"."+COLUMN_ARTIST_ID+
+            " ORDER BY "+
+            TABLE_ARTISTS +"."+COLUMN_ARTIST_NAME+", "+
+            TABLE_ALBUMS +"."+COLUMN_ALBUM_ID+", "+
+            TABLE_SONGS +"."+COLUMN_SONG_TRACK;
+
+
     private Connection connection;
 
     public boolean open() {
@@ -198,6 +214,33 @@ public class Datasource {
 
         } catch (SQLException e) {
             System.out.println("Query failed: " + e.getMessage());
+        }
+    }
+
+    public int getCount(String table){
+        String sql = "SELECT COUNT(*) AS count, MIN(_id) AS min_id FROM "+table;
+        try(Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql)){
+            int count = resultSet.getInt("count");
+            int min = resultSet.getInt("min_id");
+
+            System.out.printf("Count = %d, Min = %d\n", count, min);
+
+            return count;
+        } catch (SQLException e) {
+            System.out.println("Query failed: " + e.getMessage());
+            return -1;
+        }
+    }
+
+    public boolean createViewForSongArtists(){
+        try(Statement statement = connection.createStatement())
+            {
+            statement.execute(CREATE_ARTIST_FOR_SONG_VIEW);
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Create view failed: " + e.getMessage());
+            return false;
         }
     }
 }
