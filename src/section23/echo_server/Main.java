3ccf71da1f7c6742176dcf0648ad7aefdbdbcc7d
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-23 22:29:38 +0100

    Section 23 - First server

---
+++ src/section23/echo_server/Main.java
@@ -0,0 +1,34 @@
+package section23.echo_server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try(ServerSocket serverSocket = new ServerSocket(5000)){
+
+            Socket socket = serverSocket.accept();
+            System.out.println("Client connected");
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            while (true){
+                String echoString = input.readLine();
+                if(echoString.equals("exit")){
+                    break;
+                }
+                output.println("Echo from the server: "+echoString);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Server exception " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+}
