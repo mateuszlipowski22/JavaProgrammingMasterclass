52a9ab0521e4c723bf9950d155d0a9b12fbfb34f
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-25 01:10:29 +0100

    Section 23 - HttpUrlConnection

--- src/section23/networking/Main.java
+++ src/section23/networking/Main.java
@@ -3,28 +3,58 @@ package section23.networking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class Main {
 
     public static void main(String[] args) {
-        try{
+        try {
             URL url = new URL("http://example.org");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setRequestProperty("User-Agent", "Chrome");
+            urlConnection.setReadTimeout(10000);
 
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.setDoOutput(true);
-            urlConnection.connect();
+            int responseCode = urlConnection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
 
-            BufferedReader inputStream = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            if(responseCode!=200){
+                System.out.println("Error reading web page");
+                return;
+            }
+
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 
             String line ="";
-            while(line!=null){
-                line = inputStream.readLine();
+            while((line = inputReader.readLine())!=null){
                 System.out.println(line);
             }
-            inputStream.close();
+            inputReader.close();
+
+//            urlConnection.setDoOutput(true);
+//            urlConnection.connect();
+//
+//            BufferedReader inputStream = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+//
+//            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+//
+//            for (Map.Entry<String, List<String>> entry : headerFields.entrySet()){
+//                String key = entry.getKey();
+//                List<String> value = entry.getValue();
+//                System.out.println("-------Key = "+key);
+//                for (String val : value) {
+//                    System.out.println("value = "+val);
+//                }
+//            }
+
+//            String line ="";
+//            while(line!=null){
+//                line = inputStream.readLine();
+//                System.out.println(line);
+//            }
+//            inputStream.close();
 
 //            URI uri=url.toURI();
 ////            URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
@@ -59,9 +89,9 @@ public class Main {
 //            System.out.println("Fragment = "+uri.getFragment());
 
         } catch (MalformedURLException e) {
-            System.out.println("URL  malformed : "+e.getMessage());
+            System.out.println("URL  malformed : " + e.getMessage());
         } catch (IOException e) {
-            System.out.println("IOException : "+e.getMessage());
+            System.out.println("IOException : " + e.getMessage());
         }
     }
 
