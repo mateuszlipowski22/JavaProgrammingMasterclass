1c55e6ae079525f615b9b005d93496da5ee3342f
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-28 21:43:27 +0200

    Section 12 - Generics #2

--- src/section12/generics_17/Main.java
+++ src/section12/generics_17/Main.java
@@ -1,25 +1,61 @@
 package section12.generics_17;
 
-record BaseballPlayer(String name, String position){}
+interface Player{}
+
+record BaseballPlayer(String name, String position) implements Player {}
+record FootballPlayer(String name, String position) implements Player {}
+
 public class Main {
 
     public static void main(String[] args) {
 
-        BaseballTeam phillies = new BaseballTeam("Philadelphia Philles");
-        BaseballTeam astros = new BaseballTeam("Houston Astros");
+        BaseballTeam phillies1 = new BaseballTeam("Philadelphia Philles");
+        BaseballTeam astros1 = new BaseballTeam("Houston Astros");
+        scoreResult(phillies1, 3, astros1, 5);
 
+        SportsTeam phillies2 = new SportsTeam("Philadelphia Philles");
+        SportsTeam astros2 = new SportsTeam("Houston Astros");
+        scoreResult(phillies2, 3, astros2, 5);
+
+        Team<BaseballPlayer> phillies = new Team<>("Philadelphia Philles");
+        Team<BaseballPlayer> astros = new Team<>("Houston Astros");
         scoreResult(phillies, 3, astros, 5);
 
         var harper = new BaseballPlayer("B Harper", "Right Fielder");
         var marsh = new BaseballPlayer("B Marsh", "Right Fielder");
         phillies.addTeamMember(harper);
         phillies.addTeamMember(marsh);
+        var guthrie = new BaseballPlayer("D Guthrie", "Center Fielder");
+        phillies.addTeamMember(guthrie);
         phillies.listTeamMembers();
+
+        SportsTeam afc1 = new SportsTeam("Adelaide Crows");
+        Team<FootballPlayer> afc = new Team<>("Adelaide Crows");
+        var tex = new FootballPlayer("Tex Walker", "Center half forward");
+        afc.addTeamMember(tex);
+
+        var rory = new FootballPlayer("Rory Laird", "Midfield");
+        afc.addTeamMember(rory);
+        afc.listTeamMembers();
 
     }
 
     public static void scoreResult(BaseballTeam team1, int score1,
                                    BaseballTeam team2, int score2){
+        String message = team1.setScore(score1, score2);
+        team2.setScore(score2, score1);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+
+    public static void scoreResult(SportsTeam team1, int score1,
+                                   SportsTeam team2, int score2){
+        String message = team1.setScore(score1, score2);
+        team2.setScore(score2, score1);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+
+    public static void scoreResult(Team team1, int score1,
+                                   Team team2, int score2){
         String message = team1.setScore(score1, score2);
         team2.setScore(score2, score1);
         System.out.printf("%s %s %s %n", team1, message, team2);
