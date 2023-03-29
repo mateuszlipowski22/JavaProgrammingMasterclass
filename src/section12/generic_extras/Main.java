bbea39cf51681a0c05d2a662ecd6a1e56e57aa6b
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-03-29 22:26:14 +0200

    Section 12 - Generic methods, wildcards

--- src/section12/generic_extras/Main.java
+++ src/section12/generic_extras/Main.java
@@ -9,22 +9,52 @@ public class Main {
 
 
         int studentCount = 10;
-        List<Student> students= new ArrayList<>();
+        List<Student> students = new ArrayList<>();
         for (int i = 0; i < studentCount; i++) {
             students.add(new Student());
         }
 
         printStudents(students);
 
-        List<Student> plaStudents= new ArrayList<>();
+        List<Student> plaStudents = new ArrayList<>();
         for (int i = 0; i < studentCount; i++) {
             plaStudents.add(new PLAStudent());
         }
 
         printStudents(plaStudents);
+
+        testList(new ArrayList<String>(List.of("Mati", "Seba")));
+        testList(new ArrayList<Integer>(List.of(1,2,4,5,6)));
+
     }
 
-    public static void printStudents(List<Student> students){
-        students.forEach(System.out::println);
+    public static void printStudents(List<? extends Student> students) {
+        students.forEach(student -> System.out.println(student.getYearStarted() + ": " + student));
     }
+
+    public static <T extends Student> void printMoreLists(List<T> students) {
+        students.forEach(student -> System.out.println(student.getYearStarted() + ": " + student));
+    }
+
+    public static void testList(List<?> list) {
+        for (var element : list) {
+            if (element instanceof String s) {
+                System.out.println("String: " + s.toUpperCase());
+            } else if (element instanceof Integer i) {
+                System.out.println("Integer: " + i.floatValue());
+            }
+        }
+    }
+
+//    public static void testList(List<String> list){
+//        for(var element : list){
+//            System.out.println("String: "+element.toUpperCase());
+//        }
+//    }
+//
+//    public static void testList(List<Integer> list){
+//        for(var element : list){
+//            System.out.println("Integer: "+element.floatValue());
+//        }
+//    }
 }
