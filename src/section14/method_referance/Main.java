10c9c8cbf096f5d8eabd5a234560afbd33bb605a
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-04-05 23:01:11 +0200

    Section 14 - Method references

--- src/section14/method_referance/Main.java
+++ src/section14/method_referance/Main.java
@@ -3,8 +3,7 @@ package section14.method_referance;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 
 class PlainOld{
@@ -32,6 +31,30 @@ public class Main {
         PlainOld plainOld = references1.get();
         System.out.println("-----");
         PlainOld[] pojo1 = seedArray(PlainOld::new, 10);
+
+        calculator((s1,s2)->s1+s2, "Hello ", "World");
+        calculator(String::concat, "Hello ", "World");
+
+        BinaryOperator<String> b1 = String::concat;
+        BiFunction<String, String, String> b2 = String::concat;
+        UnaryOperator<String> u1 = String::toUpperCase;
+
+        System.out.println(b1.apply("Hello ", "World"));
+        System.out.println(b2.apply("Hello ", "World"));
+        System.out.println(u1.apply("Hello "));
+
+        String result = "Hello".transform(u1);
+        System.out.println("Result = "+result);
+
+        result = result.transform(String::toLowerCase);
+        System.out.println("Result = "+result);
+
+        Function<String, Boolean> f0 = String::isEmpty;
+        boolean resultBoolean = result.transform(f0);
+        System.out.println("Result = "+resultBoolean);
+
+
+
     }
 
     private static <T> void calculator(BinaryOperator<T> finction, T value1, T value2){
