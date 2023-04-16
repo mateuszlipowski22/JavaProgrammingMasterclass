1925e577730e4c93686e872087ac34fa21e3a71e
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-04-16 21:07:04 +0200

    Section 32 - Classes Part 1

---
+++ src/section32/classes/Car.java
@@ -0,0 +1,12 @@
+package section32.classes;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+
+}
---
+++ src/section32/classes/Main.java
@@ -0,0 +1,14 @@
+package section32.classes;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car porshe = new Car();
+        Car holden = new Car();
+
+//        porshe.model="Carrera";
+
+    }
+
+}
