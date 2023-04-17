40a9b1647cf61e11d10cdc1e222375d742f84d70
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-04-17 23:18:11 +0200

    Section 32 - Inheritance Challenge Part 2

---
+++ src/section32/inheritance_challenge/Main.java
@@ -0,0 +1,15 @@
+package section32.inheritance_challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.acclerate(30);
+        outlander.acclerate(20);
+        outlander.acclerate(-42);
+
+    }
+
+}
--- src/section32/inheritance_challenge/Outlander.java
+++ src/section32/inheritance_challenge/Outlander.java
@@ -8,4 +8,27 @@ public class Outlander extends Car{
         super("Outlander", "4WD", 4, 5, 6, false);
         this.roadServiceMonths = roadServiceMonths;
     }
+
+    public void acclerate(int rate){
+
+        int newVelocity = getCurrentVelocity()+rate;
+
+        if(newVelocity==0){
+            stop();
+        }else if(newVelocity>0 && newVelocity<=10){
+            setCurrentGear(1);
+        }else if(newVelocity>10 && newVelocity<=20){
+            setCurrentGear(2);
+        }else if(newVelocity>20 && newVelocity<=30){
+            setCurrentGear(3);
+        }else{
+            setCurrentGear(4);
+        }
+
+        if(newVelocity>0){
+            changeVelocity(newVelocity, getCurrentDirection());
+        }
+
+    }
+
 }
