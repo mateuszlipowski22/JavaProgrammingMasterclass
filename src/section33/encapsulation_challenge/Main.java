1367417603d54b9f538eb0e8c88ab1e77d192845
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-04-18 02:33:54 +0200

    Section 33 - Encapsulation (+Challenge Exercise)

---
+++ src/section33/encapsulation_challenge/Main.java
@@ -0,0 +1,15 @@
+package section33.encapsulation_challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer(50, false);
+        System.out.println("initial page count = "+printer.getPagesPrinted());
+        int printedPages = printer.printPages(4);
+        System.out.println("Pages printed was "+ printedPages + " new total print count for printers " + printer.getPagesPrinted());
+        printedPages = printer.printPages(2);
+        System.out.println("Pages printed was "+ printedPages + " new total print count for printers " + printer.getPagesPrinted());
+
+    }
+
+}
