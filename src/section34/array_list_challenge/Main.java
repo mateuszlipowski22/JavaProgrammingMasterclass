b9ef220eed9ab384cd4cd6d6003cdf653ddd392a
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-04-22 22:31:39 +0200

    Section 34 - ArrayList Challenge Part 1

---
+++ src/section34/array_list_challenge/Main.java
@@ -0,0 +1,4 @@
+package section34.array_list_challenge;
+
+public class Main {
+}
---
+++ src/section34/array_list_challenge/MobilePhone.java
@@ -0,0 +1,50 @@
+package section34.array_list_challenge;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
+    public boolean addNewContact(Contact contact) {
+        if (findContact(contact.getName())>=0) {
+            System.out.println("Contact is already on file");
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
+
+    public boolean updateContact(Contact oldContact, Contact newContact){
+        int findPosition = findContact(oldContact);
+        if(findPosition<0){
+            System.out.println(oldContact.getName()+", was not found.");
+            return false;
+        }
+        this.myContacts.set(findPosition, newContact);
+        System.out.println(oldContact.getName()+", was replaced by "+newContact.getName());
+        return true;
+    }
+
+    private int findContact(Contact contact) {
+        return this.myContacts.indexOf(contact);
+    }
+
+    private int findContact(String contactName) {
+        Contact contactToFind = myContacts
+                .stream()
+                .filter(contact -> contact.getName().equals(contactName))
+                .findFirst().orElse(null);
+        if (contactToFind!=null){
+            return findContact(contactToFind);
+        }else {
+            return -1;
+        }
+    }
+}
