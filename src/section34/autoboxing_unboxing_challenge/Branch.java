945d0d39fd8c018745cb69b996039ae8541797f2
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-04-23 21:48:22 +0200

    Section 34 - Autoboxing & Unboxing (Challenge Exercise) - Part 1

---
+++ src/section34/autoboxing_unboxing_challenge/Branch.java
@@ -0,0 +1,44 @@
+package section34.autoboxing_unboxing_challenge;
+
+import java.util.ArrayList;
+
+public class Branch {
+
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean newCustomer(String customerName, double initialAmount){
+        if(findCustomer(customerName) == null){
+            this.customers.add(new Customer(customerName, initialAmount));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String customerName, double amount){
+        Customer existingCustomer = findCustomer(customerName);
+        if(existingCustomer != null){
+            existingCustomer.addTransactions(amount);
+            return true;
+        }
+        return false;
+    }
+
+    private Customer findCustomer(String customerName){
+        for (Customer checkedCustomer : this.customers) {
+            if (checkedCustomer.getName().equals(customerName)) {
+                return checkedCustomer;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/section34/autoboxing_unboxing_challenge/Main.java
@@ -0,0 +1,9 @@
+package section34.autoboxing_unboxing_challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
