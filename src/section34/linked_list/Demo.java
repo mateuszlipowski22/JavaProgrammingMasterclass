676c1a439f670d315325a0b09c90f6ac4bc1b878
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-04-24 02:01:51 +0200

    Section 34 - LinkedList Part 3

--- src/section34/linked_list/Demo.java
+++ src/section34/linked_list/Demo.java
@@ -3,26 +3,40 @@ package section34.linked_list;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Demo {
 
     public static void main(String[] args) {
         LinkedList<String> placesToVisit = new LinkedList<>();
-        placesToVisit.add("Sydney");
-        placesToVisit.add("Melbourne");
-        placesToVisit.add("Brisbane");
-        placesToVisit.add("Perth");
-        placesToVisit.add("Canberra");
-        placesToVisit.add("Adelaide");
-        placesToVisit.add("Darwin");
+        addInOrder(placesToVisit,"Sydney");
+        addInOrder(placesToVisit,"Melbourne");
+        addInOrder(placesToVisit,"Brisbane");
+        addInOrder(placesToVisit,"Perth");
+        addInOrder(placesToVisit,"Canberra");
+        addInOrder(placesToVisit,"Adelaide");
+        addInOrder(placesToVisit,"Darwin");
+        addInOrder(placesToVisit,"Darwin");
 
         printList(placesToVisit);
 
-        placesToVisit.add(1, "Alice Springs");
-        printList(placesToVisit);
+//        placesToVisit.add("Sydney");
+//        placesToVisit.add("Melbourne");
+//        placesToVisit.add("Brisbane");
+//        placesToVisit.add("Perth");
+//        placesToVisit.add("Canberra");
+//        placesToVisit.add("Adelaide");
+//        placesToVisit.add("Darwin");
+//
+//        printList(placesToVisit);
+//
+//        placesToVisit.add(1, "Alice Springs");
+//        printList(placesToVisit);
+//
+//        placesToVisit.remove(4);
+//        printList(placesToVisit);
 
-        placesToVisit.remove(4);
-        printList(placesToVisit);
+        visit(placesToVisit);
     }
 
     public static void printList(LinkedList<String> linkedList) {
@@ -52,5 +66,68 @@ public class Demo {
         }
         stringListIterator.add(newCity);
         return true;
+    }
+
+    private static void visit(LinkedList<String> cities){
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean goingForward=true;
+        ListIterator<String> listIterator = cities.listIterator();
+
+        if(cities.isEmpty()){
+                return;
+        }else {
+            System.out.println("Now visiting "+listIterator.next());
+            printMenu();
+        }
+        while (!quit){
+            int action = scanner.nextInt();
+            scanner.nextLine();
+            switch (action){
+                case 0:
+                    System.out.println("Holiday over");
+                    quit=true;
+                    break;
+                case 1:
+                    if(!goingForward){
+                        if(listIterator.hasNext()){
+                            listIterator.next();
+                        }
+                        goingForward=true;
+                    }
+                    if(listIterator.hasNext()){
+                        System.out.println("Now visiting "+listIterator.next());
+                    }else {
+                        System.out.println("Reached the end of list");
+                        goingForward=false;
+                    }
+                    break;
+                case 2:
+                    if(goingForward){
+                        if(listIterator.hasPrevious()){
+                            listIterator.previous();
+                        }
+                        goingForward=false;
+                    }
+                    if (listIterator.hasPrevious()){
+                        System.out.println("Now visiting "+listIterator.previous());
+                    }else {
+                        System.out.println("Reached the start of list");
+                        goingForward=true;
+                    }
+                    break;
+                case 3:
+                    printMenu();
+                    break;
+            }
+        }
+    }
+
+    private static void printMenu(){
+        System.out.println("Available actions: \n press");
+        System.out.println("0 - exit");
+        System.out.println("1 - go to next city");
+        System.out.println("2 - go to previous city");
+        System.out.println("3 - print main menu");
     }
 }
